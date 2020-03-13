a62c697fd5b5f65a919190686eb1fd44fa1009f5
Nicolas D <nicolas_delperdange@hotmail.com>
2020-03-13 11:00:33 +0100

    feat(openapi): add muted params + bump deps

--- src/components/Swagger.js
+++ src/components/Swagger.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from "react"
-import { SwaggerUIBundle, SwaggerUIStandalonePreset } from "swagger-ui-dist"
+import React, { useEffect } from "react";
+import { SwaggerUIBundle, SwaggerUIStandalonePreset } from "swagger-ui-dist";
 
-const DOM_ID = "swagger-ui"
-const swaggerUrl = "https://api.sencrop.com/v1/openAPI"
+const DOM_ID = "swagger-ui";
+const swaggerUrl = "https://api.sencrop.com/v1/openAPI?mutedParameters=X-APP-Version&mutedParameters=X-SDK-Version&mutedParameters=X-API-Version";
 
 const Swagger = () => {
   useEffect(() => {
@@ -11,13 +11,13 @@ const Swagger = () => {
       dom_id: `#${DOM_ID}`,
       presets: [
         SwaggerUIBundle.presets.apis,
-        SwaggerUIStandalonePreset.slice(1),
+        SwaggerUIStandalonePreset.slice(1)
       ],
       plugins: [SwaggerUIBundle.plugins.DownloadUrl],
-      layout: "StandaloneLayout",
-    })
-  }, [])
-  return <div id={DOM_ID} />
-}
+      layout: "StandaloneLayout"
+    });
+  }, []);
+  return <div id={DOM_ID} />;
+};
 
-export default Swagger
+export default Swagger;
