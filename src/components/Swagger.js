a7eaad5a8a6d705e9d09162f397b5f44dd5782b7
Nicolas D <nicolas_delperdange@hotmail.com>
2020-07-15 14:16:38 +0200

    feat(swagger): customize swagger ui
    - Add empty tags filter
    - Add tagsSorter
    - Add operationId + deeplink
    - Filter endpoints shown by the tag partnerAccess

--- src/components/Swagger.js
+++ src/components/Swagger.js
@@ -3,7 +3,34 @@ import { SwaggerUIBundle, SwaggerUIStandalonePreset } from "swagger-ui-dist";
 
 const DOM_ID = "swagger-ui";
 const swaggerUrl =
-  "https://api.sencrop.com/v1/openAPI?mutedParameters=X-APP-Version,X-SDK-Version,X-API-Version";
+  "https://api.sencrop.com/v1/openAPI?mutedParameters=X-APP-Version,X-SDK-Version,X-API-Version&tags=partnerAccess";
+
+const TAGS_ORDER = {
+  system: 1,
+  users: 2,
+  organisations: 3,
+  devices: 4,
+  oauth2: 5,
+  partners: 6,
+  data: 7
+};
+
+const HideEmptyTagsPlugin = () => {
+  return {
+    statePlugins: {
+      spec: {
+        wrapSelectors: {
+          taggedOperations: ori => (...args) => {
+            return ori(...args).filter(
+              tagMeta =>
+                tagMeta.get("operations") && tagMeta.get("operations").size > 0
+            );
+          }
+        }
+      }
+    }
+  };
+};
 
 const Swagger = () => {
   useEffect(() => {
@@ -14,8 +41,13 @@ const Swagger = () => {
         SwaggerUIBundle.presets.apis,
         SwaggerUIStandalonePreset.slice(1)
       ],
-      plugins: [SwaggerUIBundle.plugins.DownloadUrl],
-      layout: "StandaloneLayout"
+      plugins: [SwaggerUIBundle.plugins.DownloadUrl, HideEmptyTagsPlugin],
+      layout: "StandaloneLayout",
+      displayOperationId: true,
+      deepLinking: true,
+      tagsSorter: (a, b) => {
+        return (TAGS_ORDER[a] || 99) - (TAGS_ORDER[b] || 99);
+      }
     });
   }, []);
   return <div id={DOM_ID} />;
